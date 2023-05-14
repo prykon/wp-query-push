5108dec89df623206b05fa2e1d10074954bf8f77
zdmc23 <191707+zdmc23@users.noreply.github.com>
2023-05-14 07:59:11 -0400

    React 18 init

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-//import ReactDOM from 'react-dom/client'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'
 import { render } from '@wordpress/element';
 import App from './App'
 import './index.css'
 
 if (process.env.NODE_ENV === "development") {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("wpquerypush")
-  );
-  /* REACT 18
-  ReactDOM.createRoot(document.getElementById('root')).render(
+  ReactDOM.createRoot(document.getElementById("wpquerypush")).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   )
-  */
 } else {
   const el = document.getElementById("wpquerypush");
   if (el) {

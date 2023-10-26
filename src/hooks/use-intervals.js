4af3fb5cd9014f958c7f7a5498b710e3b4f58b32
Prykon <dariomanoukian@gmail.com>
2023-10-26 11:46:50 -0300

    Added a PR reference comment

--- src/hooks/use-intervals.js
+++ src/hooks/use-intervals.js
@@ -1,5 +1,5 @@
 import useSWR from "swr";
 import { fetcher } from "@/helpers";
 
-const useIntervals = () => useSWR("../wp-json/wp-query-push/v1/intervals", fetcher);
+const useIntervals = () => useSWR("../wp-json/wp-query-push/v1/intervals", fetcher); // Reason for double dots: https://github.com/zdmc23/wp-query-push/pull/9
 export default useIntervals;

dd8162dde908b58437bef9774a7cc29d506e1669
Prykon <dariomanoukian@gmail.com>
2024-03-21 22:14:12 -0300

    Removed debugging log

--- src/hooks/use-queries.js
+++ src/hooks/use-queries.js
@@ -2,5 +2,4 @@ import useSWR from "swr";
 import { fetcher } from "@/helpers";
 import { BASE_URL } from "@/constants";
 const useQueries = () => useSWR(`${BASE_URL}/queries`, fetcher);
-console.log(useQueries);
 export default useQueries;
